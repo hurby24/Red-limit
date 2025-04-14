52d73457c2fa52e5fdff8992249b45f6ea22c7fc
Hebib Qurbanov <qurbanovhebib554@gmail.com>
2025-04-15 00:58:57 +0400

    fix: format fixed window and token bucket

--- scripts/fixed_window.lua
+++ scripts/fixed_window.lua
@@ -3,10 +3,10 @@
 -- returns: currentCount or -1 if rate limit exceeded
 
 local function fixed_window(keys, args)
-  local key             = keys[1]           -- Unique identifier for the rate limiter (e.g. user ID or IP)
-  local maxRequests     = tonumber(args[1]) -- The maximum allowed requests within the time window
-  local timeWindow      = tonumber(args[2]) -- The time window in milliseconds
-  local incrementBy     = tonumber(args[3]) -- The increment for each request (typically 1)
+  local key          = keys[1]           -- Unique identifier for the rate limiter (e.g. user ID or IP)
+  local maxRequests  = tonumber(args[1]) -- The maximum allowed requests within the time window
+  local timeWindow   = tonumber(args[2]) -- The time window in milliseconds
+  local incrementBy  = tonumber(args[3]) -- The increment for each request (typically 1)
   -- Increment the counter by the specified increment value
   local currentCount = redis.call("INCRBY", key, incrementBy)
 
@@ -15,7 +15,7 @@ local function fixed_window(keys, args)
     -- Set the expiration time for the key to the defined time window
     redis.call("PEXPIRE", key, timeWindow)
 
-  -- If the current count exceeds the allowed maximum requests
+    -- If the current count exceeds the allowed maximum requests
   elseif currentCount > maxRequests then
     -- Reset the count to -1 to indicate rate limit exceeded
     currentCount = -1
--- scripts/token_bucket.lua
+++ scripts/token_bucket.lua
@@ -3,57 +3,57 @@
 -- returns: remainingTokens, nextAvailableTime
 
 local function token_bucket(keys, args)
-    local key            = keys[1]           -- Unique identifier for the rate limiter (e.g. user ID or IP)
-    local maxTokens      = tonumber(args[1]) -- Max token capacity of the bucket
-    local refillInterval = tonumber(args[2]) -- Time window (ms) after which tokens are refilled
-    local refillRate     = tonumber(args[3]) -- Number of tokens added per refill intervall
-    local now            = tonumber(args[4]) -- Current timestamp in milliseconds
-    local cost           = tonumber(args[5]) -- Tokens to consume for this request
-    
-    local bucket = redis.call("HMGET", key, "tokens", "refilledAt")
-    
-    local refilledAt
-    local tokens
-    
-    if bucket[1] == false then
-      -- If the bucket doesn't exist, initialize it
-      tokens = maxTokens
-      refilledAt = now
-    else
-      -- If the bucket exists, retrieve its values
-      tokens = tonumber(bucket[1])
-      refilledAt = tonumber(bucket[2])
-    end
-    
-    -- Calculate the number of refills that have occurred since the last request
-    if now >= refilledAt + refillInterval then
-      local numRefills = math.floor((now - refilledAt) / refillInterval)
-      tokens = math.min(maxTokens, tokens + numRefills * refillRate)
-    
-      -- Update the last refill time
-      refilledAt = refilledAt + numRefills * refillInterval
-    end
-    
-    -- If there are no tokens available, return -1 and the time when the next token will be available
-    if tokens == 0 then
-      return {-1, refilledAt + refillInterval}
-    end
-    
-    local remainingTokens = tokens - cost
+  local key            = keys[1]           -- Unique identifier for the rate limiter (e.g. user ID or IP)
+  local maxTokens      = tonumber(args[1]) -- Max token capacity of the bucket
+  local refillInterval = tonumber(args[2]) -- Time window (ms) after which tokens are refilled
+  local refillRate     = tonumber(args[3]) -- Number of tokens added per refill intervall
+  local now            = tonumber(args[4]) -- Current timestamp in milliseconds
+  local cost           = tonumber(args[5]) -- Tokens to consume for this request
 
-    -- If the remaining tokens are less than 0, return -1 and the time when the next token will be available
-    if remainingTokens < 0 then
-      return {-1, refilledAt + refillInterval}
-    end
+  local bucket         = redis.call("HMGET", key, "tokens", "refilledAt")
 
-    -- Calculate the expiration time for the bucket based on the remaining tokens
-    local expireAt = math.ceil(((maxTokens - remainingTokens) / refillRate)) * refillInterval
-    
-    -- Update the bucket with the new values
-    redis.call("HSET", key, "refilledAt", refilledAt, "tokens", remainingTokens)
-    redis.call("PEXPIRE", key, expireAt)
+  local refilledAt
+  local tokens
 
-    return {remainingTokens, refilledAt + refillInterval}
+  if bucket[1] == false then
+    -- If the bucket doesn't exist, initialize it
+    tokens = maxTokens
+    refilledAt = now
+  else
+    -- If the bucket exists, retrieve its values
+    tokens = tonumber(bucket[1])
+    refilledAt = tonumber(bucket[2])
+  end
+
+  -- Calculate the number of refills that have occurred since the last request
+  if now >= refilledAt + refillInterval then
+    local numRefills = math.floor((now - refilledAt) / refillInterval)
+    tokens = math.min(maxTokens, tokens + numRefills * refillRate)
+
+    -- Update the last refill time
+    refilledAt = refilledAt + numRefills * refillInterval
+  end
+
+  -- If there are no tokens available, return -1 and the time when the next token will be available
+  if tokens == 0 then
+    return { -1, refilledAt + refillInterval }
+  end
+
+  local remainingTokens = tokens - cost
+
+  -- If the remaining tokens are less than 0, return -1 and the time when the next token will be available
+  if remainingTokens < 0 then
+    return { -1, refilledAt + refillInterval }
+  end
+
+  -- Calculate the expiration time for the bucket based on the remaining tokens
+  local expireAt = math.ceil(((maxTokens - remainingTokens) / refillRate)) * refillInterval
+
+  -- Update the bucket with the new values
+  redis.call("HSET", key, "refilledAt", refilledAt, "tokens", remainingTokens)
+  redis.call("PEXPIRE", key, expireAt)
+
+  return { remainingTokens, refilledAt + refillInterval }
 end
 
 redis.register_function('token_bucket', token_bucket)
